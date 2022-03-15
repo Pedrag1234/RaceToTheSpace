4d01c32619caa4b060fbbcd47b1a2464ff88e0d6
Pedro Azevedo <pedrag1234@gmail.com>
2022-03-15 20:22:21 +0000

    feat: Level now resets when player gets hit by bullet

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BulletBehaviour : MonoBehaviour
 {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
