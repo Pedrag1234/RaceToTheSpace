8b6fed9768a4c61497f02c5b50948623ed705bd1
nunosanto <up201405774@up.pt>
2022-03-16 21:08:45 +0000

    motion capture camera with door

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -4,18 +4,56 @@ using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float m_Range = 5f;
+
+    [SerializeField]
+    private Transform m_Target;
+    
+    [SerializeField]
+    private DoorSetActive door;
+
+    private Vector2 m_TargetDirection;
+
+    public bool playerDetected = false;
+
 
     // Update is called once per frame
     void Update()
     {
-        transform.position=player.transform.position + new Vector3(5,2.5f,-10);
+
+        m_TargetDirection = m_Target.position - transform.position;
+
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, m_TargetDirection, m_Range);
+
+        if (ray.collider != null)
+        {
+            if(ray.collider.gameObject.tag == "Player")
+            {
+                if (!playerDetected)
+                {
+                    playerDetected = true;
+                }
+                    
+            }
+        }
+        else
+        {
+            if (playerDetected)
+            {
+                playerDetected = false;
+
+            }
+        }
+
+        if (playerDetected)
+        {
+            gameObject.transform.up = m_TargetDirection;
+            door.OpenDoor();
+        }
+        else
+            door.CloseDoor();
     }
 }
+
