4e45a9e304d7b068fde96ee5dba0b0c974deb296
nunosanto <up201405774@up.pt>
2022-03-16 09:36:03 +0000

    door and button floor

--- Assets/Scripts/Triggers/DoorButton.cs
+++ Assets/Scripts/Triggers/DoorButton.cs
@@ -7,8 +7,14 @@ public class DoorButton : MonoBehaviour
     [SerializeField] private DoorSetActive door;
 
     private void OnTriggerEnter2D(Collider2D collider){
-        if(collider.GetComponent<CharacterController>()!=null){
+        if(collider.tag == "Player"){
             door.OpenDoor();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider){
+        if(collider.tag == "Player"){
+            door.CloseDoor();
         }
     }
 }
