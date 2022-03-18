73c00411f9554b175d2b8eb73d88b004bf2f25f4
Mariana <up201605775@g.uporto.pt>
2022-03-18 00:53:41 +0000

    feat: player camera

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position=player.transform.position + new Vector3(5,2.5f,-10) ;
+    }
+}
