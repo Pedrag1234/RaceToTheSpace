55317e45bc5526e05c2169aacd61e6f4b7e8bbeb
Mariana <up201605775@g.uporto.pt>
2022-03-18 01:16:50 +0000

    feat: timer done

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCamera : MonoBehaviour
 {
     [SerializeField]
     private GameObject player;
+
+    //UI text
+    public Text timerText;
+
+    public float secondsCount;
+    public int minuteCount;
+    public int hourCount;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +24,21 @@ public class PlayerCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position=player.transform.position + new Vector3(5,2.5f,-10) ;
+        transform.position=player.transform.position + new Vector3(5,2.5f,-10);
+
+        UpdateTimerUI();
+    }
+
+    //update timer 
+    public void UpdateTimerUI(){
+         secondsCount += Time.deltaTime;
+         timerText.text = hourCount +"H "+ minuteCount +"M "+(int)secondsCount + "S";
+         if(secondsCount >= 60){
+             minuteCount++;
+             secondsCount = 0;
+         }else if(minuteCount >= 60){
+             hourCount++;
+             minuteCount = 0;
+         }    
     }
 }
